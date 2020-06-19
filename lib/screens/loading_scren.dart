2582e34b01eeaab25184ad29d50cbe56add368bb
kabbo.nibir <kabbo.nibir@gmail.com>
2020-06-19 23:28:54 +0600

    up

--- lib/screens/loading_scren.dart
+++ lib/screens/loading_scren.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:weatherapp/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
