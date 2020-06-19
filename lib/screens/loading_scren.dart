2cd5145e6cdc07bb13a276e4f710b457b9f34521
kabbo.nibir <kabbo.nibir@gmail.com>
2020-06-19 23:27:14 +0600

    up

--- lib/screens/loading_scren.dart
+++ lib/screens/loading_scren.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:weatherapp/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,15 +13,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-
     getLocation();
-
   }
 
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  Location location = Location();
+  await location.getCurrentLocation();
+  print(location.latitude);
+  print(location.longitude);
   }
 
   @override
